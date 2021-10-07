d707b036c374ac31e73d8af354fb4703d308345b
Antonio Terceiro <antonio.terceiro@linaro.org>
2021-10-07 17:15:15 -0300

    Add basic reusable gitlab CI pipeline
    This basic pipeline can be used to build packages and create a
    repository from them.

--- tuxpkg/__main__.py
+++ tuxpkg/__main__.py
@@ -3,6 +3,7 @@ import sys
 import tuxpkg
 import tuxpkg.get_makefile
 import tuxpkg.get_debian_rules
+import tuxpkg.create_repository
 
 
 def main():
@@ -35,6 +36,13 @@ def main():
         help="Prints the path to the tuxpkg shared debian/rules. It can be included in a your debian/rules using a construct like like `$(include $(shell tuxpkg get-makefile))`. You just need to set PYBUILD_NAME first.",
     )
     get_debian_rules.set_defaults(func=tuxpkg.get_debian_rules.run)
+
+    create_repository = subparsers.add_parser(
+        "create-repository",
+        aliases=["repo"],
+        help="Creates Debian and RPM repository from files in dist/."
+    )
+    create_repository.set_defaults(func=tuxpkg.create_repository.run)
 
     options = parser.parse_args(sys.argv[1:])
     options.func()
